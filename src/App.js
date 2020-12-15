70439526760c5eadbca008cda0d8ad50fb950f52
Eric Kudler <eric.kudler@hearst.com>
2020-12-15 00:34:33 -0500

    Created a card to use as a placeholder for the layour

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Container, Row, Col } from 'react-bootstrap';
 import NavigationBar from './common/NavigationBar';
+import InputCard from './common/cards/InputCard';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-      
+      <Container>
+        <Row>
+          <Col md={5}>
+            <InputCard />
+          </Col>
+          <Col md={7}>
+            <InputCard />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/common/cards/InputCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Card, Row, Col } from 'react-bootstrap';
+
+//import './InputCard.css';
+
+class InputCard extends Component {
+  render() {
+
+    return (
+      <div>
+        <Card>
+          <Card.Body>
+            Hey there
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default InputCard;
