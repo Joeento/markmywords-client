2536c9bf34cdef1738ccd574bf5f8866bb4b8741
Eric Kudler <eric.kudler@hearst.com>
2020-12-15 20:40:52 -0500

    Added a card for showing markov cards

---
+++ src/common/cards/MarkovCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Card, Row, Col, Form, Button } from 'react-bootstrap';
+
+import './MarkovCard.css';
+
+class MarkovCard extends Component {
+  render() {
+    return (
+      <Card className="markov-card">
+        <Card.Body>
+          <span className="result-text">{this.props.result}</span>
+          <br />
+          <div className="float-right">
+          <small className="text-muted"> 5 minutes ago</small>
+          </div>
+        </Card.Body>
+        <Card.Footer className="text-muted">
+          <strong>Original: </strong> {this.props.original}
+        </Card.Footer>
+      </Card>
+    );
+  }
+}
+
+export default MarkovCard;
