21af158809c8da61e59f3084f1216ae2883569f0
Eric Kudler <eric.kudler@hearst.com>
2020-12-17 17:12:43 -0500

    Added some dummy cards to show what it will look like when it's finished

--- src/common/cards/ResultsCard.js
+++ src/common/cards/ResultsCard.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import { Card, Row, Col, Form, Button } from 'react-bootstrap';
 import NumericInput from 'react-numeric-input';
 
-//import './ResultsCard.css';
+import MarkovCard from './MarkovCard';
+
+import './ResultsCard.css';
 
 class ResultsCard extends Component {
   render() {
     return (
-      <div>
-        <Card>
-          <Card.Body>
-            this is where the results will go.
-          </Card.Body>
-        </Card>
-      </div>
+      <Card className="results-card">
+        <Card.Body>
+          <MarkovCard
+            result="Dorothy thought, were lovely patches of the room, now here, or I should have not like a roof, which made it up and as they turned their friend, although they were being rocked gently, like it. He worked hard that she wore a cradle."
+            original="Dorothy lived in the midst of the great Kansas prairies, with Uncle Henry, who was a farmer, and Aunt Em, who was the farmer's wife... (see more)" />
+          <MarkovCard
+            result="the other black beetle bled only black blood but the other black beetle bled only black blood but the other black beetle bled blue"
+            original="One black beetle bled only black blood but the other black beetle bled blue" />
+          <MarkovCard
+            result="the theirs, ok? yes, ok? yes, it is a theremin is, ok? yes, theremin is, this a theremin is a theremin."
+            original="the theremin is theirs, ok? yes, it is, this is a theremin." />
+
+          <MarkovCard
+            result="theirs, it is, theremin."
+            original="the theremin is theirs, ok? yes, it is, this is a theremin." />
+        </Card.Body>
+      </Card>
     );
   }
 }
